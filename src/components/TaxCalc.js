5758ac228dde9208e4481980d08e36ea19cc04ce
w3villa-anchal <anchal.gupta@w3villa.com>
2022-07-19 13:59:56 +0530

    updated nav bar

--- src/components/TaxCalc.js
+++ src/components/TaxCalc.js
@@ -1,7 +1,5 @@
 import React, { useRef } from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 // import { render } from 'react-dom';
 
 import InputField from './inputfield';
@@ -19,27 +17,12 @@ export default function TaxCalc(props) {
     
   return (
     <Container>
-    <Row>
-        <Col sm={8} className="column1">
-        </Col> 
-        <Col sm={4} className="column2"> 
-            <Row className="row1">
-                <form ref={nameForm} align="center">
-                    <InputField label={'amount'} name={'amount'}/>
-                    <InputField label={'rate'} name={'rate'}/>
-                </form>
-                <button onClick={handleClickEvent}>Get value</button>
-            </Row>
-            <Row className="row2">
-                2nd row
-            </Row>
-            <Row className="row3">
-                3rd row
-            </Row>
-        
-        </Col>
+        <form ref={nameForm} align="center">
+            <InputField label={'amount'} name={'amount'}/>
+            <InputField label={'rate'} name={'rate'}/>
+        </form>
+        <button onClick={handleClickEvent}>Get value</button>
 
-    </Row>
     </Container>
   )
 }
--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Feedback from '../Feedback';
 import Info from '../Info';
 import TaxCalc from '../TaxCalc';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 export default function Main() {
@@ -18,8 +21,23 @@ export default function Main() {
   return (
     <div className='main'>
     <Info name={info}/>
-    <Feedback/>
-    <TaxCalc values={values}/>
+    <Container>
+    <Row>
+        <Col sm={8} className="column1">
+        </Col> 
+        <Col sm={4} className="column2"> 
+            <Row className="row1">
+            <TaxCalc values={values}/>
+            </Row>
+            <Row className="row2">
+              <Feedback/>
+            </Row>
+            <Row className="row3">
+                3rd row
+            </Row>
+        </Col>
+    </Row>
+    </Container>
     </div>
   )
 }
